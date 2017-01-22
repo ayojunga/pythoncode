b870aa9f1ca24343c3d79e80c629977edccd2a2d
Yunus Umarov <umarovjunes@yahoo.com>
2017-01-22 23:04:29 +0000

    added comments to recipeconverter.py

--- recipeconverter.py
+++ recipeconverter.py
@@ -7,7 +7,7 @@ yeast = input("Enter the amount of yeast (teaspoons):")
 factor = input("Enter the loaf adjustment factor (e.g 2.0 double the size):")
 
 print "\n--Modified Recipe --"
-
+#multiples the user input with the loaf adjustment factor that they picked
 print "Bread flour: %.1f cups" % (float(flour)*factor)
 print "Water: %.1f cups" % (float(water)*factor)
 print "Salt: %.1f teaspoons" % (float(salt)*factor)
@@ -15,7 +15,7 @@ print "Yeast: %.1f teaspoons" % (float(yeast)*int(factor))
 print "Happy Baking!"
 
 print "\n--Modified Recipe in Grams --"
-
+#multiples the variables with their corresponding grams
 print "Bread flour: %.2f g." % (float(flour)*factor*120)
 print "Water: %.2f g." % (float(water)*factor*237)
 print "Salt: %.2f g." % (float(salt)*factor*5)
