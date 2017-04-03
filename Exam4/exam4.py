3badebf669deed4c9a494700d5681985ae62a758
Yunus Umarov <umarovjunes@yahoo.com>
2017-04-03 13:00:53 +0000

    upload

---
+++ Exam4/exam4.py
@@ -0,0 +1,51 @@
+import func
+import os
+import string
+
+def readFiles():
+    
+    fileList = []
+    
+    files = os.listdir(".")
+    #adds all the files that end in .txt into an array of files
+    for fname in files:
+        
+        if '.txt' in fname:
+            
+            fileList.append(fname)
+    
+    return fileList
+
+
+def searchFile(fname, word):
+	count = 0
+	#reads the file and removes all punctuaion from each line and separates the word
+	for line in open(fname, "r"):
+		no_punc = line.translate(None, string.punctuation)
+		words = line.split(" ")
+		#uppercases each line and prints the file name and the line
+		for w in words:
+			if word in w.upper():
+				count += 1
+				print '{}: {}'.format(fname, line.upper().strip())
+
+	return count
+	
+	
+	
+
+def main():
+	
+	fileList = readFiles()
+	word = func.userString("Enter a search term:").upper()
+	count = 0
+	
+	#for each file it counts the total amount of times it saw the word and runs the function fileList
+	for file in fileList:
+		count += searchFile(file, word)
+
+	print "\nI found {} results" .format(count)
+
+	
+
+main()
---
+++ Exam4/func.py
@@ -0,0 +1,18 @@
+def userInt(prompt):
+    print prompt,
+    num = int(raw_input())
+    return num
+
+def userString(prompt):
+    print prompt,
+    s = raw_input()
+    return s
+
+def userFloat(prompt):
+    print prompt,
+    f = float(raw_input())
+    return f
+    
+def readTemp():
+    temp = readTemp()
+    return temp
